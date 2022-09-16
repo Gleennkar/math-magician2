2af7a66d42a55e643fb46ceb4a722cc174763118
Odette Gleen <nkargleeny@gmail.com>
2022-09-16 21:55:26 +0100

    add event listener

--- src/App.js
+++ src/App.js
@@ -2,12 +2,6 @@ import React from 'react';
 import Calculator from './components/Calculator';
 import './components/Calculator.css';
 
-// const App = () => (
-//   <>
-//     <Calculator />
-//   </>
-// );
-
 export default class App extends React.PureComponent {
   render() {
     return (
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,34 +1,53 @@
 import './Calculator.css';
 import React from 'react';
+// import './calculate.js';
+// import './operate.js';
+import calculate from '../logic/calculate';
 
 export default class Calculator extends React.PureComponent {
-  render() {
-    return (
-      <div className="calculator-grid">
-        <div className="output">
-          <div className="operand">0</div>
-        </div>
-        <button type="button" className="button">AC</button>
-        <button type="button" className="button">+/-</button>
-        <button type="button" className="button">%</button>
-        <button type="button" className="symbol">÷</button>
-        <button type="button" className="button">7</button>
-        <button type="button" className="button">8</button>
-        <button type="button" className="button">9</button>
-        <button type="button" className="symbol">&times;</button>
-        <button type="button" className="button">4</button>
-        <button type="button" className="button">5</button>
-        <button type="button" className="button">6</button>
-        <button type="button" className="symbol">-</button>
-        <button type="button" className="button">1</button>
-        <button type="button" className="button">2</button>
-        <button type="button" className="button">3</button>
-        <button type="button" className="symbol">+</button>
-        <button type="button" className="span-two">0</button>
-        <button type="button" className="button">.</button>
-        <button type="button" className="symbol">=</button>
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.handleClick = this.handleclick.bind(this);
+  }
 
-      </div>
+  handleClick(event) {
+    this.setState((state) => calculate(state, event.target.textContent));
+  }
+
+  render() {
+    const { next, toatal } = this.state; // eslint-disable-line
+    return (
+      <>
+
+        <div className="calculator-grid">
+          <div className="output">
+            <span className="result">{next || total || 0}</span>
+            <div className="operand">0</div>
+          </div>
+          <button type="button" className="button" onClick={this.handleClick}>AC</button>
+          <button type="button" className="button" onClick={this.handleClick}>+/-</button>
+          <button type="button" className="button" onClick={this.handleClick}>%</button>
+          <button type="button" className="symbol active" onClick={this.handleClick}>÷</button>
+          <button type="button" className="button" onClick={this.handleClick}>7</button>
+          <button type="button" className="button" onClick={this.handleClick}>8</button>
+          <button type="button" className="button" onClick={this.handleClick}>9</button>
+          <button type="button" className="symbol active" onClick={this.handleClick}>&times;</button>
+          <button type="button" className="button" onClick={this.handleClick}>4</button>
+          <button type="button" className="button" onClick={this.handleClick}>5</button>
+          <button type="button" className="button" onClick={this.handleClick}>6</button>
+          <button type="button" className="symbol active" onClick={this.handleClick}>-</button>
+          <button type="button" className="button" onClick={this.handleClick}>1</button>
+          <button type="button" className="button" onClick={this.handleClick}>2</button>
+          <button type="button" className="button" onClick={this.handleClick}>3</button>
+          <button type="button" className="symbol active" onClick={this.handleClick}>+</button>
+          <button type="button" className="span-two" onClick={this.handleClick}>0</button>
+          <button type="button" className="button" onClick={this.handleClick}>.</button>
+          <button type="button" className="symbol active" onClick={this.handleClick}>=</button>
+
+        </div>
+
+      </>
     );
   }
 }
